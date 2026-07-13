5ec65ac7bf631c0f08f5b31062a4f13e2233c525
Alaa Elgazwy <ai6949347@gmail.com>
2026-07-13 19:06:26 +0300

    fixed problem page not found

--- Day-03-Docker/app.js
+++ Day-03-Docker/app.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('DevOps Deployment Success 🚀');
+  res.send('مرحباً بك! هذا التطبيق يعمل بنجاح داخل Docker 🐳');
 });
 
-app.get('/health', (req, res) => {
-    res.json({ status: 'OK' });
-});
 
-app.listen(3000,'0.0.0.0', () => {
-    console.log('Server running on port 3000');
+app.listen(port, '0.0.0.0', () => {
+  console.log(`App is running on http://0.0.0.0:${port}`);
 });
